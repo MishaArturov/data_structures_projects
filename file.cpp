dafd0debb6dd67e04abb3e3a2aedbeb5483460e6
U-BSD\479574 <479574@SHS-9YBF6X3.beaverton.k12.or.us>
2025-09-05 09:56:54 -0700

    added guessing Game

--- file.cpp
+++ file.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 int main(){
-  std::cout<<"Hello World";
+  srand(time(NULL));
+  char play = 'y';
+  while(play=='y'){
+  int secretNum = rand()%100+1;
+  int Guess = -1;
+  //cout<<"the number is "<<secretNum<<endl;
+  while(Guess!=secretNum){
+    cout<<"Enter a number 1-100 ";
+    cin>>Guess;
+    if(Guess<secretNum){
+      cout<<"too low"<<endl;
+    }
+    else if(Guess>secretNum){
+      cout<<"too high"<<endl;
+    }
+  }
+  cout<<"Congrats! you guessed the number"<<endl<<"Do you want to play again? (y/n) "<<endl;
+  cin>>play;
+  }
+  cout<<"Thanks for playing the Guessing game";
 }
