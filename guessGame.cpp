2a90598c98ae2172728478c22e1dd11e93d9b88e
U-BSD\479574 <479574@SHS-9YBF6X3.beaverton.k12.or.us>
2025-09-05 10:15:47 -0700

    added comments

--- guessGame.cpp
+++ guessGame.cpp
@@ -3,24 +3,25 @@
 #include <ctime>
 using namespace std;
 int main(){
-  srand(time(NULL));
-  char play = 'y';
-  while(play=='y'){
-  int secretNum = rand()%100+1;
-  int Guess = -1;
+  srand(time(NULL)); //generates random seed
+  char play = 'y'; //character that confims player wants to play again, yes on start
+  while(play=='y'){ //first loop that makes the game run multiple times
+    int secretNum = rand()%100+1; //secre number player guesses
+    int Guess = -1; //the integer that is used to store the guesses
   //cout<<"the number is "<<secretNum<<endl;
-  while(Guess!=secretNum){
+    while(Guess!=secretNum){ //second loop for guesses
     cout<<"Enter a number 1-100 ";
-    cin>>Guess;
-    if(Guess<secretNum){
+    cin>>Guess; //takes players number
+    if(Guess<secretNum){ //checs if number too low
       cout<<"too low"<<endl;
     }
-    else if(Guess>secretNum){
+    else if(Guess>secretNum){  //checks if number too igh
       cout<<"too high"<<endl;
     }
   }
-  cout<<"Congrats! you guessed the number"<<endl<<"Do you want to play again? (y/n) "<<endl;
+    cout<<"Congrats! you guessed the number"<<endl<<"Do you want to play again? (y/n) "<<endl; //runs when 2nd loop ends and asks to \
+play again
   cin>>play;
   }
-  cout<<"Thanks for playing the Guessing game";
+  cout<<"Thanks for playing the Guessing game"; //runs when first loop ends
 }
