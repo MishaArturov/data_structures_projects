7e92632771ee63d6542527d2a7df052028d1eb09
MishaArturov <479574@bsd48.org>
2025-11-17 01:40:08 -0800

    Update ClassesProj.cpp
    added virtual print function and construct for video game

--- ClassesProj.cpp
+++ ClassesProj.cpp
@@ -7,12 +7,26 @@ class DigitalMedia {
     public:
     char title[30];
     int year;
+    virtual void print() { //virtual function to print the childrens contents
+        
+    }
 };
 
 class VideoGame : public DigitalMedia {
     public:
     char publisher[30];
     int rating;
+    VideoGame(char t[30], int y, char p[30], int r) {//constructs the videogame with all needed stuff
+        strcpy(title, t);
+        year = y;
+        strcpy(publisher, p);
+        rating = r;
+    }
+
+    void print() override { //virtural func print of videogame
+        cout << "Video Game: " << title << ", Year: " << year
+             << ", Publisher: " << publisher << ", Rating: " << rating << endl;
+    }
 };
 
 class Music : public DigitalMedia {
