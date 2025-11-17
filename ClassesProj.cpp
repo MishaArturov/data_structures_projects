4fc6ac5645b326dd2c49068cc5e26f7321bf8eaf
MishaArturov <479574@bsd48.org>
2025-11-17 00:35:41 -0800

    Update ClassesProj.cpp
    Made all classes within the main ClassesProj

--- ClassesProj.cpp
+++ ClassesProj.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+class DigitalMedia {
+
+};
+
+class VideoGame : public DigitalMedia {
+
+};
+
+class Music : public DigitalMedia {
+
+};
+
+class Movie : public DigitalMedia {
+
+};
+
+int main() {
+
+    return 0;
+}
