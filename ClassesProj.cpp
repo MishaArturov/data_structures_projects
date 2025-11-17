55d675e0c7e699a29ee21f5acbb57240a1d0d590
MishaArturov <479574@bsd48.org>
2025-11-17 01:23:09 -0800

    Update ClassesProj.cpp
    added the fields for each class

--- ClassesProj.cpp
+++ ClassesProj.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 class DigitalMedia {
-
+    public:
+    char title[30];
+    int year;
 };
 
 class VideoGame : public DigitalMedia {
-
+    public:
+    char publisher[30];
+    int rating;
 };
 
 class Music : public DigitalMedia {
-
+    public:
+    char publisher[30];
+    int duration;
+    char artist[30];
 };
 
 class Movie : public DigitalMedia {
-
+    public:
+    char director[30];
+    int duration;
+    int rating;
 };
 
 int main() {
