7754828857cb4c5262d1c2f046e4a472cf9e3eee
MishaArturov <479574@bsd48.org>
2025-11-17 01:45:05 -0800

    Update ClassesProj.cpp
    added the constructs for the rest of child classes

--- ClassesProj.cpp
+++ ClassesProj.cpp
@@ -31,9 +31,23 @@ class VideoGame : public DigitalMedia {
 
 class Music : public DigitalMedia {
     public:
+    char artist[30];
     char publisher[30];
     int duration;
-    char artist[30];
+
+    Music(char t[30], int y, char a[30], int d, char p[30]) {
+        strcpy(title, t);
+        year = y;
+        strcpy(artist, a);
+        duration = d;
+        strcpy(publisher, p);
+    }
+
+    void print() override {
+        cout << "Music: " << title << ", Year: " << year
+             << ", Artist: " << artist << ", Duration: " << duration
+             << " secs, Publisher: " << publisher << endl;
+    }
 };
 
 class Movie : public DigitalMedia {
@@ -41,6 +55,20 @@ class Movie : public DigitalMedia {
     char director[30];
     int duration;
     int rating;
+
+    Movie(char t[30], int y, char d[30], int dur, int r) {
+        strcpy(title, t);
+        year = y;
+        strcpy(director, d);
+        duration = dur;
+        rating = r;
+    }
+
+    void print() override {
+        cout << "Movie: " << title << ", Year: " << year
+             << ", Director: " << director << ", Duration: " << duration
+             << " mins, Rating: " << rating << endl;
+    }
 };
 
 int main() {
