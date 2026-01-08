e33c01ac6aa1d6798a0262d927a76cf4f6181a28
MishaArturov <479574@bsd48.org>
2026-01-08 01:39:27 -0800

    Update Linked_List_main.cpp
    main tests that node and student functions work

--- linked_list/Linked_List_main.cpp
+++ linked_list/Linked_List_main.cpp
@@ -37,6 +37,18 @@ class Node{
 };
 int main()
 {
-
+    Student* s1 = new Student(4);
+    Student* s2 = new Student(12);
+    Student* s3 = new Student(67);
+    Node* n1 = new Node(s1);
+    Node* n2 = new Node(s2);
+    Node* n3 = new Node(s3);
+    n1->setNext(n2);
+    n2->setNext(n3);
+    cout<<n1->getStudent()->getID()<<endl;
+    cout<<n1->getNext()->getStudent()->getID()<<endl;
     return 0;
+    delete n1;
+    delete n2;
+    delete n3;
 }
