2488aa314d6a471a86cbe88a73f3abf6d9c9255f
MishaArturov <479574@bsd48.org>
2026-01-08 01:22:49 -0800

    Update Linked_List_main.cpp
    simple student class

--- linked_list/Linked_List_main.cpp
+++ linked_list/Linked_List_main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
+class student{
+    private:
+        int id;
+    public:
+    student(int id){
+        this->id = id;
+    }
+    int getID(){
+        return id;
+    }
+};
 int main()
 {
-    
+
     return 0;
 }
