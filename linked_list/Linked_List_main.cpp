6fbfbfc030ffed5b065467bf61c0b4721c1fcda5
MishaArturov <479574@bsd48.org>
2026-01-08 01:30:49 -0800

    Update Linked_List_main.cpp
    simple node class

--- linked_list/Linked_List_main.cpp
+++ linked_list/Linked_List_main.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
-class student{
+class Student{
     private:
         int id;
     public:
-    student(int id){
+    Student(int id){
         this->id = id;
     }
     int getID(){
         return id;
     }
+};
+class Node{
+    private:
+        Node* next;//pointer to next node in list
+        Student* stu;//pointer to data aka the student
+    public:
+        Node(Student* s){
+            this->stu = s;
+            this->next = nullptr;//null next on construction
+        }
+        Student* getStudent(){
+            return stu;
+        }
+        Node* getNext(){
+            return next;
+        }
+        ~Node(){
+            delete stu;
+            next = nullptr;
+        }
 };
 int main()
 {
