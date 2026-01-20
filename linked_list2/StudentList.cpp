3752198e7136625deb2a0cf6aa737f525be61d3d
MishaArturov <479574@bsd48.org>
2026-01-20 01:49:56 -0800

    Update StudentList.cpp
    updated the main funciton

--- linked_list2/StudentList.cpp
+++ linked_list2/StudentList.cpp
@@ -96,37 +96,44 @@ void deleteStudentRecur(Node*& head, int id) {
 
 
 
-// void addStudent
-int main()
-{
+int main() {
     Node* head = nullptr;
-    while(true){
-        cout<<"Use one of the commands (ADD,PRINT,DELETE,QUIT)"<<endl;
 
-        
-        
-        char arr[10]; //checks command
-        cin>>arr;
-        for(int x=0; x<strlen(arr);x++){//turns into upperacse
+    while (true) {
+        cout << "Use one of the commands (ADD, PRINT, DELETE, AVERAGE, QUIT)" << endl;
+
+        char arr[10];
+        cin >> arr;
+
+        for (int x = 0; x < strlen(arr); x++) {//makes command uppercase
             arr[x] = toupper(arr[x]);
         }
-        if(strcmp(arr,"ADD")==0){
-            addStudent(studentPtrs);;
+
+        if (strcmp(arr, "ADD") == 0) {
+            addStudent(head);
         }
-        else if (strcmp(arr,"DELETE")==0){
-            deleteStudent(studentPtrs);;
+        else if (strcmp(arr, "DELETE") == 0) {
+            deleteStudent(head);
         }
-        else if (strcmp(arr,"PRINT")==0){
-            printStudent(studentPtrs);;
+        else if (strcmp(arr, "PRINT") == 0) {
+            printStudent(head);
         }
-        else if (strcmp(arr,"QUIT")==0){
+        else if (strcmp(arr, "AVERAGE") == 0) {
+            printAverage(head);
+        }
+        else if (strcmp(arr, "QUIT") == 0) {
             break;
         }
+    }
 
+    // frees data before exit
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->getNext();
+        delete temp->getStudent();
+        delete temp;
     }
-    for (Student* s : studentPtrs){//frees data
-        delete s;
-    }
-    studentPtrs.clear();
+
     return 0;
 }
+
