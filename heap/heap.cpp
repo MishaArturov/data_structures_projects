369b568b634901fb06b31ff04072f0e287e6a034
MishaArturov <479574@bsd48.org>
2026-03-04 04:30:37 -0800

    added insert ability

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -13,5 +13,27 @@ public:
     MaxHeap() {
         size = 0;
     }
+
+    bool isFull() {
+        return size == 100;
+    }
+
+
+    void insert(int value) {
+        if (isFull()) {//returns if array size reached
+            cout << "Heap is full.\n";
+            return;
+        }
+
+        size++;
+        heap[size] = value;
+
+        // Heapify up
+        int current = size;
+        while (current > 1 && heap[current] > heap[current / 2]) {//swaps child with parent until sorted
+            swap(heap[current], heap[current / 2]);
+            current = current / 2;
+        }
+    }
 };
 
