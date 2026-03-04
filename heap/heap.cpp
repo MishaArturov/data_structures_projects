881e5aba1515b79be1c7ad9384a17584268f43e8
MishaArturov <479574@bsd48.org>
2026-03-04 04:25:20 -0800

    created heap class

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+class MaxHeap {
+private:
+    int heap[101];
+    int size;
+
+public:
+    MaxHeap() {
+        size = 0;
+    }
+};
 
