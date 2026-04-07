d1a3272f9a33d663222e54a30484712414bb01a9
MishaArturov <479574@bsd48.org>
2026-04-07 00:53:23 -0700

    stack functions

--- ShuntingYard/shuntingYard.cpp
+++ ShuntingYard/shuntingYard.cpp
@@ -19,3 +19,19 @@ struct Node {
         next = nullptr;
     }
 };
+void push(Node** top, Node* newNode) {
+    newNode->next = *top;
+    *top = newNode;
+}
+
+Node* pop(Node** top) {
+    if (*top == nullptr) return nullptr;
+    Node* temp = *top;//temporary copy
+    *top = (*top)->next;//top is shifted down
+    temp->next = nullptr;//prints the temp
+    return temp;
+}
+
+Node* peek(Node* top) {//just returns top
+    return top;
+}
